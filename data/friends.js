8b503b8fef6706e4a30140a278732773d99f452a
May Ewers <mestipul@gmail.com>
2019-05-05 22:17:07 -0400

    homework

---
+++ data/friends.js
@@ -0,0 +1,182 @@
+var friendMatch = [
+
+    {
+        name: 'Mario',
+        photo: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/bb/8b/eb/bb8beb7b-a7c6-ce90-ef4a-b81cd10d1524/AppIcon-0-1x_U007emarketing-0-85-220-6.png/246x0w.jpg',
+        scores: [
+           1,
+           5,
+           3,
+           4,
+           5,
+           3,
+           2,
+           5,
+           4,
+           2
+        ]
+    },
+
+    {
+        name: 'Luigi',
+        photo: 'https://i.redd.it/rs7ew3qulzny.png',
+        scores: [
+          5,
+          1,
+          2,
+          4,
+          3,
+          1,
+          4,
+          3,
+          5,
+          2
+
+        ]
+    },
+
+    {
+        name: 'Princess Peach',
+        photo: 'https://i.pinimg.com/236x/2f/e1/ab/2fe1ab0826ea0b78ec43d9bf25770306.jpg',
+        scores: [
+          5,
+          4,
+          2,
+          1,
+          3,
+          4,
+          2,
+          3,
+          1,
+          5
+        ]
+    },
+
+    {
+        name: 'Bowser',
+        photo: 'https://vignette.wikia.nocookie.net/ironyoshi1212rockz/images/d/d1/Character-bowser.png/revision/latest?cb=20160112162332',
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+
+    {
+        name: "Yoshi",
+        photo: 'http://assets1.ignimgs.com/2017/06/13/yoshibr-1497339410648_1280w.jpg',
+        scores: [
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4
+        ]
+    },
+
+    {
+        name: 'Toad',
+        photo: 'https://cdn.vox-cdn.com/thumbor/xWkrJATSPVEJy6BiiYChC2tN_x0=/0x0:1200x1200/1200x800/filters:focal(509x287:701x479)/cdn.vox-cdn.com/uploads/chorus_image/image/61448237/tum.0.jpg',
+        scores: [
+            4,
+            4,
+            4,
+            4,
+            4,
+            3,
+            3,
+            3,
+            5,
+            5
+
+        ]
+    },
+
+    {
+        name: 'Rosalina',
+        photo: 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/rosalina-super-mario-galaxy-44.9.jpg',
+        scores: [
+          2,
+          2,
+          5,
+          3,
+          1,
+          4,
+          2,
+          4,
+          2,
+          1
+
+        ]
+    },
+
+    {
+        name: 'Koopa',
+        photo: 'https://vignette.wikia.nocookie.net/newersupermariobroswii/images/1/10/Koopa_Paratroopa_%28New_Super_Mario_Bros%29.png/revision/latest?cb=20180304160638',
+        scores: [
+          5,
+          5,
+          5,
+          2,
+          2,
+          5,
+          4,
+          1,
+          2,
+          5
+
+        ]
+    },
+
+
+    {
+        name: 'Princess Daisy',
+        photo: 'https://banner2.kisspng.com/20180417/scq/kisspng-princess-daisy-super-mario-bros-princess-peach-donkey-kong-5ad5c185d4c646.2229195415239581498715.jpg',
+        scores: [
+           4,
+           3,
+           4,
+           1,
+           5,
+           5,
+           3,
+           1,
+           1,
+           2
+
+        ]
+    },
+
+
+    {
+        name: 'Birdo',
+        photo: 'http://mariopartylegacy.com/wp-content/uploads/2011/08/birdoprofile.png',
+        scores: [
+            4,
+            2,
+            2,
+            5,
+            5,
+            1,
+            1,
+            1,
+            3,
+            5,
+
+        ]
+    }
+]
+
+module.exports = friendMatch;
